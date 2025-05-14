025ea61380f5824b6db3627783b9d6f7461ac04d
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-14 11:42:36 +0400

    Updated Python script to demo conditional statements

--- main_app.py
+++ main_app.py
@@ -1,14 +1,27 @@
 # Declaration
 first_number = 0  # Lower Case
 second_number = 0
+operation = ""
 result = 0
 
 # Take Input
 first_number = int(input("Please enter the 1st number: "))  # "10" -> 10
 second_number = int(input("Please enter the 2nd number: "))  # "20" -> 20
+operation = input("Please enter one of +  -  *  or /: ")
 
-# Process the Input 
-result = first_number + second_number
+# Processing Input
+if operation == "+":
+   result = first_number + second_number
+elif operation == "-":
+   result = second_number - first_number
+elif operation == "*":
+   result = first_number * second_number
+elif operation == "/" and second_number != 0:
+    result = first_number / second_number
+else:
+   print("[WARN] Operation not supported!")
 
 # Output
 print("Result is: ", result, "!!!")
+
+print("End of Script")
