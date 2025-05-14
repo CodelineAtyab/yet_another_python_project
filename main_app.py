325a4a80dd2bf700b1bee1215087642fa703aa22
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-14 09:07:45 +0400

    simple calc with add functionality

---
+++ main_app.py
@@ -0,0 +1,14 @@
+# Declaration
+first_number = 0  # Lower Case
+second_number = 0
+result = 0
+
+# Take Input
+first_number = int(input("Please enter the 1st number: "))  # "10" -> 10
+second_number = int(input("Please enter the 2nd number: "))  # "20" -> 20
+
+# Process the Input 
+result = first_number + second_number
+
+# Output
+print("Result is: ", result, "!!!")
