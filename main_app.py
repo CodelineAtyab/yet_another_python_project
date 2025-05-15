77b4b00064d36e2e9673cbf80fb921a0d4374ac8
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-15 09:32:39 +0400

    Added pow functionality

--- main_app.py
+++ main_app.py
@@ -7,7 +7,7 @@ result = 0
 # Take Input
 first_number = int(input("Please enter the 1st number: "))  # "10" -> 10
 second_number = int(input("Please enter the 2nd number: "))  # "20" -> 20
-operation = input("Please enter one of +  -  * / or mod: ")
+operation = input("Please enter one of +  -  * / mod or pow: ")
 
 # Processing Input
 if operation == "+":
@@ -20,6 +20,8 @@ elif operation == "/" and second_number != 0:
     result = first_number / second_number
 elif operation == "mod" and second_number != 0:
    result = first_number % second_number
+elif operation == "pow":
+   result = first_number ** second_number
 else:
    print("[WARN] Operation not supported!")
 
