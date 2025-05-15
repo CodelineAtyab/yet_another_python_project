9e53ee863da3e08e2de68cfb788f1fb7c6137621
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-15 09:08:56 +0400

    Added mod functionality

--- main_app.py
+++ main_app.py
@@ -7,7 +7,7 @@ result = 0
 # Take Input
 first_number = int(input("Please enter the 1st number: "))  # "10" -> 10
 second_number = int(input("Please enter the 2nd number: "))  # "20" -> 20
-operation = input("Please enter one of +  -  *  or /: ")
+operation = input("Please enter one of +  -  * / or mod: ")
 
 # Processing Input
 if operation == "+":
@@ -18,6 +18,8 @@ elif operation == "*":
    result = first_number * second_number
 elif operation == "/" and second_number != 0:
     result = first_number / second_number
+elif operation == "mod" and second_number != 0:
+   result = first_number % second_number
 else:
    print("[WARN] Operation not supported!")
 
