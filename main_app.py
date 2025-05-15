0f971e92549e3a31af214b321411f44f2e1a7352
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-15 10:01:13 +0400

    Added factorial

--- main_app.py
+++ main_app.py
@@ -2,12 +2,12 @@
 first_number = 0  # Lower Case
 second_number = 0
 operation = ""
-result = 0
+result = 1
 
 # Take Input
 first_number = int(input("Please enter the 1st number: "))  # "10" -> 10
 second_number = int(input("Please enter the 2nd number: "))  # "20" -> 20
-operation = input("Please enter one of +  -  * / mod pow or abs: ")
+operation = input("Please enter one of +  -  * / mod pow abs and fac: ")
 
 # Processing Input
 if operation == "+":
@@ -24,6 +24,9 @@ elif operation == "pow":
    result = first_number ** second_number
 elif operation == "abs":
    result = abs(first_number)
+elif operation == "fac":
+   for count in range(1, first_number + 1):
+      result *= count
 else:
    print("[WARN] Operation not supported!")
 
