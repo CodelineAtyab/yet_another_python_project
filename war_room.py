a540be5ad4d1d712e2172682885ba1076950a17d
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-18 09:18:43 +0400

    Initial file for the new calc

---
+++ war_room.py
@@ -0,0 +1,19 @@
+"""
+This script is going to become a calculator with multiple features
+Multiple team members are going to work here together
+To deliver different features
+"""
+# Declaration
+num_1 = 0
+num_2 = 0
+result = 0
+
+# Input
+num_1 = input("Enter 1st Number: ")
+num_2 = input("Enter 2nd Number: ")
+
+# Process
+
+
+# Output
+print("Result is:", result)
